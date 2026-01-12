dcff2805cc72c4f1b496c58bedeeb4465776e851
Quentin Smith <quentinsmith3006@gmail.com>
2026-01-12 12:21:03 -0500

    Added various exports to qchess module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,4 +4,12 @@ module QChess {
     requires javafx.graphics;
     requires org.controlsfx.controls;
     requires org.jetbrains.annotations;
+
+    exports qchess.chess.chessmen;
+    exports qchess.chess.create.direction;
+    exports qchess.chess.create.interfaces;
+    exports qchess.chess.create.piecemodifiers;
+    exports qchess.chess.create;
+    exports qchess.chess.logic.event;
+    exports qchess.chess.logic;
 }
