2928aab189abe4f8fdd4e4ea8af64f281834ed5d
Quentin Smith <quentinsmith3006@gmail.com>
2026-01-12 12:36:15 -0500

    Module stuff

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,15 @@
-module QChess {
+module qchess.chess {
     requires javafx.base;
     requires javafx.controls;
     requires javafx.graphics;
     requires org.controlsfx.controls;
     requires org.jetbrains.annotations;
+
+    exports qchess.chess.chessmen;
+    exports qchess.chess.create.direction;
+    exports qchess.chess.create.interfaces;
+    exports qchess.chess.create.piecemodifiers;
+    exports qchess.chess.create;
+    exports qchess.chess.logic.event;
+    exports qchess.chess.logic;
 }
